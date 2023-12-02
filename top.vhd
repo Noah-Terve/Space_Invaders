262f3ae0cd0ded5ec0ca8fd98698084df2bffa07
Braun Lippe <44823019+braunzl@users.noreply.github.com>
2023-12-01 19:10:52 -0500

    Update top.vhd

--- top.vhd
+++ top.vhd
@@ -2,101 +2,165 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity top is
-    port(
-        nes_data           : in std_logic;
-	    nes_clk            : out std_logic;
-		temp  			   : out std_logic_vector (7 downto 0);
-        nes_latch          : out std_logic;
+entity top is 
+	port (
+		input_clock : in std_logic; -- Input clock
+		HSYNC : out std_logic; -- HSYNC to VGA
+		VSYNC : out std_logic; -- VSYNC to VGA
+		rgb : out std_logic_vector(5 downto 0); -- RGB output to VGA
 		
-		disp 			   : out std_logic_vector(6 downto 0);
-		top_two 		   : out std_logic_vector(1 downto 0);
-		led 			   : out std_logic_vector(1 downto 0)
-    );
+		
+		nes_clk : out std_logic;
+		nes_data : in std_logic;
+		nes_latch : out std_logic
+		
+	);
 end top;
 
 architecture synth of top is
 
-signal clk     			  : std_logic;
-signal nes_result  		  : std_logic_vector (7 downto 0);
-signal valid   			  : std_logic := '0';
+component mypll is
+    port(
+        ref_clk_i: in std_logic; -- Input clock
+        rst_n_i: in std_logic; -- Reset (active low)
+        outcore_o: out std_logic; -- Output to pins
+        outglobal_o: out std_logic -- Output for clock network
+    );
+end component;
+
+component vga is
+	port(
+		clk : in std_logic; -- Clock
+		valid : out std_logic; -- Bit stating whether in visible area.
+		row : out unsigned(9 downto 0); -- Given row of VGA image.
+		col : out unsigned(9 downto 0); -- Given col of VGA image.
+		HSYNC : out std_logic; -- HSYNC to VGA.
+		VSYNC : out std_logic -- VSYNC to VGA. 
+	);
+end component;
+
+component pattern_gen is
+	port (
+		clk : in std_logic;
+		valid : in std_logic; -- Valid bit for VGA.
+		row : in unsigned(9 downto 0); -- Given row of VGA display.
+		col : in unsigned(9 downto 0); -- Given col of VGA display.
+		rgb : out std_logic_vector(5 downto 0); -- RGB color to VGA display.
+		player_x : in unsigned(9 downto 0);
+		player_y : in unsigned(9 downto 0)
+	);
+end component;
+
+component controller is 
+	port(
+        in_clk : in std_logic;
+	    controller_clk : out std_logic;
+        latch : out std_logic;
+        data : in std_logic;
+        result : out std_logic_vector(7 downto 0)
+    );
+end component;
+
+component game_logic is 
+	port(
+	    given_clk             : in std_logic;
+        controller_data       : in std_logic_vector (7 downto 0);
+
+        -- position data
+        aliens                : out std_logic_vector (19 downto 0);
+        alienx                : out unsigned (9 downto 0);
+        alieny                : out unsigned (8 downto 0);
+        
+        playerx               : out unsigned (9 downto 0);
+        playery               : out unsigned (8 downto 0);
+
+        bulletx               : out unsigned (9 downto 0);
+        bullety               : out unsigned (8 downto 0);
+        bulletvalid           : out std_logic
+    );
+end component;
+		
+signal outcore_o : std_logic;
+signal outglobal_o : std_logic;
+signal clk : std_logic;
+signal valid : std_logic;
+signal row : unsigned(9 downto 0);
+signal col : unsigned(9 downto 0);
+
+signal player_x : unsigned(9 downto 0);
+signal player_y : unsigned(9 downto 0);
+
+signal nes_result : std_logic_vector(7 downto 0);
+
 signal logic_aliens       : std_logic_vector (19 downto 0);
 signal logic_alienx       : unsigned (9 downto 0);
 signal logic_alieny       : unsigned (8 downto 0);
-        
-signal logic_playerx      : unsigned (9 downto 0);
-signal logic_playery      : unsigned (8 downto 0);
 
 signal logic_bulletx      : unsigned (9 downto 0);
 signal logic_bullety      : unsigned (8 downto 0);
 signal logic_bulletvalid  : std_logic;
 
-
-component dddd is
-    port(
-        display_clk : in std_logic;
-        position : in unsigned(7 downto 0);
-        display_val : out std_logic_vector(6 downto 0);
-        leds : out std_logic_vector(1 downto 0)
-
-    );
-end component;
-
-
--- END TESTING --
-component HSOSC is
-	generic (CLKHF_DIV : String := "0b00"); -- Divide 48MHz clock by 2^N (0-3)
-	port(
-		CLKHFPU : in std_logic := 'X'; -- Set to 1 to power up
-		CLKHFEN : in std_logic := 'X'; -- Set to 1 to enable output
-		CLKHF : out std_logic := 'X'); -- Clock output
-end component;
-
-component controller is
-    port(
-        in_clk : in std_logic := '0';
-	    controller_clk : out std_logic := '0';
-        latch : out std_logic := '0';
-        data : in std_logic := '0';
-        result : out std_logic_vector(7 downto 0) := 8b"0"
-    );
-end component;
-
-component game_logic is
-    port(
-	    given_clk             : in std_logic := '0';
-        controller_data       : in std_logic_vector (7 downto 0) := 8b"0";
-
-        -- position data
-        aliens                : out std_logic_vector (19 downto 0) :=  20b"11111111111111111111";
-        alienx                : out unsigned (9 downto 0) := 10d"31";
-        alieny                : out unsigned (8 downto 0) := 9d"432";
-        
-        playerx               : out unsigned (9 downto 0) := 10d"240";
-        playery               : out unsigned (8 downto 0) := 9d"61";
-
-        bulletx               : out unsigned (9 downto 0) := 10b"0";
-        bullety               : out unsigned (8 downto 0) := 9b"0";
-        bulletvalid           : out std_logic := '0'
-    );
-end component;
+signal temp : unsigned (9 downto 0);
 
 begin
-    osc : HSOSC generic map ( CLKHF_DIV => "0b00")
-                   port map (CLKHFPU => '1',
-                             CLKHFEN => '1',
-                             CLKHF => clk);
-	nes : controller port map ( in_clk => clk, controller_clk => nes_clk, latch => nes_latch, data => nes_data, result => nes_result);
-    logic : game_logic port map (
-        given_clk => nes_clk, controller_data => nes_result,
-        aliens => logic_aliens, alienx => logic_alienx, alieny => logic_alieny,
-        playerx => logic_playerx, playery => logic_playery,
-        bulletx => logic_bulletx, bullety => logic_bullety, bulletvalid => logic_bulletvalid
-    );
-	display : dddd port map (display_clk => clk, position => logic_bullety(7 downto 0), display_val => disp, leds => led);
-	temp <= nes_result;
-	top_two(0) <= logic_bullety(8);
-	top_two(1) <= logic_bulletvalid;
+	pll : mypll
+		port map (
+			ref_clk_i => input_clock,
+			rst_n_i => '1',
+			outcore_o => outcore_o,
+			outglobal_o => outglobal_o
+		);
+		
+	myvga : vga
+		port map (
+			outglobal_o,
+			valid,
+			row,
+			col,
+			HSYNC,
+			VSYNC
+		);
+	
+	my_pattern_gen : pattern_gen
+		port map (
+			outglobal_o,
+			valid,
+			row,
+			col,
+			rgb,
+			player_x,
+			player_y
+		);
+		
+	new_controller : controller 
+		port map (
+			in_clk => outglobal_o,
+			controller_clk => nes_clk,
+			latch => nes_latch, 
+			data => nes_data,
+			result => nes_result
+		);
+		
+	space_invader_game_logic : game_logic
+		port map (
+			given_clk => nes_clk,
+			controller_data => nes_result,
 
-    -- create instances of the various needed modules and map their ports appropriately
-end synth;
+			-- position data
+			aliens => logic_aliens,
+			alienx => logic_alienx,
+			alieny => logic_alieny,
+			
+			playerx => player_x,
+			playery => temp,
+
+			bulletx => logic_bulletx,
+			bullety => logic_bullety,
+			bulletvalid => logic_bulletvalid
+		);
+		
+	
+		
+	-- player_x <= to_unsigned(10, 10);
+	player_y <= to_unsigned(419, 10);
+end;
